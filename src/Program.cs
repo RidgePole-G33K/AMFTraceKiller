f83cb8b060016f4a726c99b033362aef94bc495b
RidgePole_G33K <60889562+RidgePole-G33K@users.noreply.github.com>
2024-04-27 18:43:37 +0800

    Add files via upload

---
+++ src/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace AMFTraceKiller
+{
+    class Program
+    {
+        static void Main()
+        {
+            var path = AppDomain.CurrentDomain.BaseDirectory + "livehime.exe";
+
+            if (!File.Exists(path))
+                return;
+
+            var waiter = new AutoResetEvent(false);
+
+            var p = new Process
+            {
+                EnableRaisingEvents = true,
+                StartInfo = new ProcessStartInfo(path)
+            };
+            p.Exited += (o, e) =>
+            {
+                if (File.Exists(path = @"C:\AMFTrace.log"))
+                    File.Delete(path);
+                waiter.Set();
+            };
+            p.Start();
+
+            waiter.WaitOne();
+        }
+    }
+}
